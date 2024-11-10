707d9aa0d9d0032be3d16617c5af225da6774e0a
Shifa <bsse1301@iit.du.ac.bd>
2024-11-10 13:21:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect, request, render_template
 import json
 
-# from game import gameRunner
+from game import GameRunner
 
 app = Flask(__name__)
-# game_runner=gameRunner()
+game_runner=GameRunner()
 
 @app.route('/')
 def index():
@@ -14,44 +14,44 @@ def index():
 def level(level):
     return render_template(f"{level}.html")
 
-# @app.route('/restart', methods=['GET'])
-# def restart():
-#     player_index = int(request.args['playerIndex'])
-#     if player_index not in [-1, 1]:
-#         status = 'Fail'
-#     else:
-#         status = 'OK'
-#         game_runner.restart(player_index)
-#     return json.dumps({'status': status})
+@app.route('/level/<string:level>/restart', methods=['GET'])
+def restart(level):
+    player_index = int(request.args['playerIndex'])
+    if player_index not in [-1, 1]:
+        status = 'Fail'
+    else:
+        status = 'OK'
+        game_runner.restart(player_index)
+    return json.dumps({'status': status})
 
 
-# @app.route('/play')
-# def play():
-#     x, y = request.args['x'], request.args['y']
-#     if game_runner.play(int(x), int(y)):
-#         status = 'OK'
-#     else:
-#         status = 'Fail'
-#     return json.dumps({'status': status, 'game_status': game_runner.get_status()})
+@app.route('/level/<string:level>/play')
+def play(level):
+    x, y = request.args['x'], request.args['y']
+    if game_runner.play(int(x), int(y)):
+        status = 'OK'
+    else:
+        status = 'Fail'
+    return json.dumps({'status': status, 'game_status': game_runner.get_status()})
 
 
-# @app.route('/aiplay')
-# def aiplay():
-#     status, move = game_runner.aiplay()
-#     if status:
-#         return json.dumps(
-#             {
-#                 'status': 'OK',
-#                 'move': {'x': int(move[0]), 'y': int(move[1])},
-#                 'game_status': game_runner.get_status()
-#             }
-#         )
-#     return json.dumps({'status': 'Fail', 'game_status': game_runner.get_status()})
+@app.route('/level/<string:level>/aiplay')
+def aiplay(level):
+    status, move = game_runner.aiplay()
+    if status:
+        return json.dumps(
+            {
+                'status': 'OK',
+                'move': {'x': int(move[0]), 'y': int(move[1])},
+                'game_status': game_runner.get_status()
+            }
+        )
+    return json.dumps({'status': 'Fail', 'game_status': game_runner.get_status()})
 
 
-# @app.route('/get_gamestate')
-# def get_gamestate():
-#     return json.dumps({'status': 'OK', 'game_status': game_runner.get_status()}) 
+@app.route('/level/<string:level>/get_gamestate')
+def get_gamestate(level):
+    return json.dumps({'status': 'OK', 'game_status': game_runner.get_status()}) 
 
 @app.after_request
 def after_request(response):
